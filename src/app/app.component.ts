e4a6e6ef37ad091fdb75bd237f3ee45e27fc7519
Juan Antonio Garcia Ruiz <ing.garciaruiz.9707@gmail.com>
2024-02-20 19:09:10 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dafn3';
+  intento = 0;
+  rechazado= true;
+
+  alternativa = [
+    "No",
+    "Andale",
+    "Por favor",
+    "Insisto",
+    "Lo pensaré",
+    "Casi me convences",
+    "Una más",
+    "..."
+  ];
+
+  columns = [
+    "grid-template-columns: 50% 50%;",
+    "grid-template-columns: 55% 45%;",
+    "grid-template-columns: 65% 35%;",
+    "grid-template-columns: 70% 30%;",
+    "grid-template-columns: 75% 25%;",
+    "grid-template-columns: 80% 20%;",
+    "grid-template-columns: 90% 10%;",
+    "grid-template-columns: 90% 10%;",
+    "grid-template-columns: 100%",
+  ];
+  
+
+  reaccion(): void {
+    this.intento++;
+  }
+
+  aceptar(): void {
+    this.rechazado = false;
+  }
 }
